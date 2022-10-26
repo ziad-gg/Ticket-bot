9ed0191759907e555abe5f73767ecaa36920b079
ziad <zemad3040@gmail.com>
2022-10-26 12:37:15 +0300

    main

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Ticket } = require('./structure/ticketBot');
 const express = require("express");
 const app = express();
-app.listen(5000);
+app.listen(process.env.PORT || 80);
 app.get("/", (req, res) => res.send("hello"))
 new Ticket(process.env.TOKEN)
 .run();
